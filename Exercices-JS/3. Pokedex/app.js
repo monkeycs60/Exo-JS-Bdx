62580afe3ccfb3e9d9fcbcb1714779fe800a372f
monkeycs60 <clement.serizay@gmail.com>
2022-05-01 19:19:09 +0200

    pokemon projet 3

--- Exercices-JS/3. Pokedex/app.js
+++ Exercices-JS/3. Pokedex/app.js
@@ -1,44 +1,59 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-const alphabetMaj = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const grid = document.querySelector('.grid-pokedex');
+let allPokemons = [];
 
-function encryptor (key, message) {
-    let minuscule = message.toLowerCase();
-    let result = [];
-for (let index = 0; index < minuscule.length; index++) {
-    const lettre = minuscule[index];
-    const lettreCode = alphabet.indexOf(lettre);
-    const lettreCodeCrypte = lettreCode + key;
-    const lettreCrypte = alphabet[lettreCodeCrypte];
-    const regex = /[^a-zA-Z0-9]/g;
+
+
     
-    if (lettre === message.match(regex)) {
-        result.push(lettre);
+    
+    function appelPoke() {
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.results.forEach(pokemon => {
+                const card = document.createElement('div');
+                card.classList.add('card');
+                card.innerHTML = `
+                    <p> ${pokemon.name}</p>
+                `;
+                grid.appendChild(card);
+            });
+    
+          })
     }
-   else if (message[index] === message[index].toUpperCase()) {
-        if ((alphabetMaj.indexOf(message[index]) + key) <= 25) {
-            result.push(alphabetMaj[(alphabetMaj.indexOf(message[index]) + key)]);
-        } 
-        else {
-            result.push(alphabetMaj[alphabetMaj.indexOf(message[index]) + key % 26]);
-        }
+    appelPoke();
+
+    function fetchPokemonComplet(pokemon) {
+        let objPokemonFull = {};    
+        let url = pokemon.url;
+        let nameP = pokemon.name;
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            objPokemonFull.pic = data.sprites.front_default;
+            objPokemonFull.type = data.types[0].name;
+            objPokemonFull.id = data.id;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                objPokemonFullName = data.names[4].name;
+                allPokemons.push(objPokemonFull);
+                if (allPokemons.length === 151) {
+                    console.log(allPokemon);
+                }
+                    
+                }
             
-        } 
-    else if (message[index] === message[index].toLowerCase()) {
-        if ((alphabet.indexOf(message[index]) + key) <= 25) {
-            result.push(alphabet[(alphabet.indexOf(message[index]) + key)]);
-        } 
-        else {
-            result.push(alphabet[alphabet.indexOf(message[index]) + key % 26]);
-        }
+
+        
+        )
     }
-    
-    
-    
-    
+    )
 }
-
-return result.join("");
-}
-
-
-console.log(encryptor(1, "I love  America"));
+fetchPokemonComplet(pokemon);
+    
+    
+    
