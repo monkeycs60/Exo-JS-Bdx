e0f653cde3923e24a0d79ff011d455fa5ac6ae9e
monkeycs60 <clement.serizay@gmail.com>
2022-05-02 19:42:10 +0200

    intégration du reste de la carte pokemon

--- Exercices-JS/3. Pokedex/app.js
+++ Exercices-JS/3. Pokedex/app.js
@@ -18,17 +18,15 @@ fetch(`https://pokeapi.co/api/v2/pokemon-species/charmander`)
             console.log(data);
             console.log(data.results);
             data.results.forEach(pokemon => {
+                console.log(pokemon.name);
                 const card = document.createElement('div');
                 card.classList.add('card');
-                card.innerHTML = `
-                    <p> ${pokemon.name}</p>
-                `;
                 grid.appendChild(card);
                 
-            
+                
                 //fetch sprite for each pokemon
                 fetch(pokemon.url)  
-
+                
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
@@ -36,25 +34,29 @@ fetch(`https://pokeapi.co/api/v2/pokemon-species/charmander`)
                     sprite.src = data.sprites.front_default;
                     card.appendChild(sprite);
                 })
+                console.log(pokemon.name);
+                //             fetch('https://pokeapi.co/api/v2/pokemon-species/pokemon.name')
+               
+   
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                   // create a const for each pokemon french name
+                    const frenchName = document.createElement('p');
+                    frenchName.innerHTML = data.names.find(name => name.language.name === 'fr').name;
+                    card.appendChild(frenchName);
+                    // create a const for each pokemon ID
+                    const id = document.createElement('p');
+                    id.innerHTML = `#${data.id}`;
+                    card.appendChild(id);
+                })
+  
             })
+            console.log(data.results.name);
                 
             });
            //fetch pokemon name in french in pokemon-species
-            fetch('https://pokeapi.co/api/v2/pokemon-species/')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-//for each pokemon-species, fetch the french name
-                data.results.forEach(pokemon => {
-                    console.log(pokemon);
-              const namePokemon = document.createElement('p');
-                namePokemon.innerHTML = pokemon.name;
-                grid.appendChild(namePokemon);
-
-
-                    })
-                }
-            )
         }
     
     appelPoke();
