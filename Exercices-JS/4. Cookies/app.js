d1cab864647ce10526a151eff2a3cfb509c958e8
monkeycs60 <clement.serizay@gmail.com>
2022-05-12 02:14:17 +0200

    finalisation détail app cookie / reste responsive et design

--- Exercices-JS/4. Cookies/app.js
+++ Exercices-JS/4. Cookies/app.js
@@ -16,37 +16,30 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 //transforùm next week's month to a number
 const nextWeekMonthNumber = months.indexOf(nextWeekMonth) + 1;
 //convert nextweekmonthnumber to an array
-console.log(nextWeekMonthNumber);
+const convert = nextWeekMonthNumber.toString().split('');
+let month = convert;
+if (convert.length > 1) {
+    month = convert[0] + convert[1];
+    console.log(month);
+} else {
+    month = `0${convert[0]}`;
+    console.log(month);
+    
+}
+//check if the array has more than 1 digit
+console.log(month);
+console.log(typeof nextWeekMonthNumber);
 
-
-
+console.log(nextWeek);
+console.log(convert);
 //log all const 
-const dateComplete = `${nextWeekYear}-0${nextWeekMonthNumber}-${nextWeekDay}`;
-console.log(dateComplete);
-console.log(dateComplete);
-
-console.log(nextWeekYear);
-console.log(nextWeekDay);
-
+const dateComplete = `${nextWeekYear}-${month}-${nextWeekDay}`;
 
 //set input3 value to the nextWeek date
 const input3 = document.querySelector('.input3');
-const inputDate = document.querySelector('input[type="date"]');
-
-console.log(input3);
-
-
 
 
 document.querySelector('.input3').value = dateComplete;
-console.log(dateComplete);
-//set input 3 value to the dateComplete with slash
-
-
-
-
-
-
 
 
 // premier bouton CREER
@@ -54,6 +47,8 @@ console.log(dateComplete);
 buttonCreate.addEventListener('click', (e) => {
     //prevent default
     e.preventDefault();
+    const list = document.querySelector('.liste-cookie');
+    list.innerHTML = '';
     document.querySelector('.info-cookie').style.display = 'block';
     const name = document.querySelector('.input1').value;
     const value = document.querySelector('.input2').value;
@@ -65,21 +60,25 @@ buttonCreate.addEventListener('click', (e) => {
     }, 5000);
     console.log(name, value, duration);
     //if name is already in the cookies then display a message in the info-cookie, that the name is already used
-    if (document.cookie.includes(name)) {
+    if (name === '' || value === '' || duration === '') {
+        document.querySelector('.info-cookie').innerHTML = 'Veuillez remplir tous les champs !';  
+    } 
+    else if (document.cookie.includes(name)) {
         document.querySelector('.info-cookie').innerHTML = 'Le nom du cookie est déjà utilisé !';
-    } else {
+    } 
+
+    else {
 
     //create a cookie with name, value and duration
     document.cookie = `${name}=${value}; expires=${new Date(duration).toUTCString()}`;
     }
-      
+     
     document.querySelector('.input1').value = '';
     document.querySelector('.input2').value = '';
-    document.querySelector('.input3').value = '';
+    document.querySelector('.input3').value = dateComplete;
 
 }
 );
-
 
 // deuxième bouton SUPPRIMER
 //addeventlistener to the button2 to display the name and the value for each cookie and their date of expiration in the ol list
@@ -94,23 +93,12 @@ button2.addEventListener('click', (e) => {
     console.log(cookies);
     for (let i = 0; i < cookies.length; i++) {
         const li = document.createElement('li');
-        li.innerHTML = `Nom : ${cookies[i].split('=')[0]} // Valeur : ${cookies[i].split('=')[1]} // expire le ${cookies[i].split('=')[2]}`;
+        li.innerHTML = `Nom : ${cookies[i].split('=')[0]} // Valeur : ${cookies[i].split('=')[1]}`;
         list.appendChild(li);
     }
 
-    console.log(cookies.length);
-
-  // if there is no cookie, then display a message in the info-cookie
-//     if (cookies.length === 1) {
-//         document.querySelector('.info-cookie').innerHTML = 'Aucun cookie !';
-//     }
-// }
-// );
 }
 );
-
-
-
 
 //addeventlistener to the li, when you click on an li it will delete the cookie
 const list = document.querySelector('.liste-cookie');
@@ -122,9 +110,6 @@ list.addEventListener('click', (e) => {
         // const name = e.target.innerHTML.split(' ')[3];
         const name = e.target.innerHTML.split(' : ')[1].split(' // ')[0];
          
-
-       
-        console.log(name);
         document.cookie = `${name}=; expires=${new Date(0).toUTCString()}`;
         e.target.remove();
     }
