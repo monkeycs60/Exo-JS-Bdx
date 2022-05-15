f6ee10c54a87e8a992f035cbab004c5768fa1a1c
monkeycs60 <clement.serizay@gmail.com>
2022-05-15 23:49:40 +0200

    pomodoro seconde

--- Exercices-JS/6. Pomodoro/app.js
+++ Exercices-JS/6. Pomodoro/app.js
@@ -1,81 +1,55 @@
-// create a p in travail, then add to it a timer of 30 minutes
-// create a p in pause, then add to it a timer of 5 minutes
-
+//create const for travail, repos, start, pause, reset, cycles
 const travail = document.querySelector('.travail');
 const repos = document.querySelector('.repos');
-const timer30 = document.createElement('p');
-const pause5 = document.createElement('p');
-
-
-// const decompte = (minutes, secondes) => {
-//     let secondesRestantes = minutes * 60 + secondes;
-//     let interval = setInterval(() => {
-//         secondesRestantes--;
-//         if (secondesRestantes >= 0) {
-//             let minutesRestantes = Math.floor(secondesRestantes / 60);
-//             let secondesRestantes = secondesRestantes % 60;
-//             if (secondesRestantes < 10) {
-//                 secondesRestantes = '0' + secondesRestantes;
-//             }
-//             if (minutesRestantes < 10) {
-//                 minutesRestantes = '0' + minutesRestantes;
-//             }
-//             timer30.textContent = `${minutesRestantes}:${secondesRestantes}`;
-//         } else {
-//             clearInterval(interval);
-//         }
-//     }, 1000);
-// };
-
-// timer30 is (datenow + 30min) - datenow;
-timer30.innerHTML = new Date((Date.now() - 1800000) - Date.now()).toLocaleTimeString().slice(3);
-travail.appendChild(timer30);
-
-pause5.innerHTML = new Date((Date.now() - 50100000) - Date.now()).toLocaleTimeString().slice(3);
-repos.appendChild(pause5);
-
-//addeventlistener to the start button, when you click on it, the timer30 is started
-//addeventlistener to the stop button, when you click on it, the timer30 is stopped
-//addeventlistener to the reset button, when you click on it, the timer30 is reseted
 const start = document.querySelector('.start');
-const stop = document.querySelector('.stop');
+const pause = document.querySelector('.pause');
 const reset = document.querySelector('.reset');
+const cycles = document.querySelector('.cycles');
 
-const uneSeconde = 1000;
-let dateInit = new Date((Date.now() - 1800000) - Date.now());
-console.log(dateInit- uneSeconde);
-console.log(new Date((Date.now() - 1800000) - Date.now()).toLocaleTimeString().slice(3));
-console.log(new Date((Date.now() - 1800000) - Date.now()) - 1000);
+//create two variables for travail and repos
+let travailTime = 10;
+let reposTime = 5;
 
-console.log(dateInit.toLocaleTimeString().slice(3));
-//addeventlistener to start button to start the timer
+// display travailTime in travail and convert it to minutes and seconds
+// add a zero before the minutes and seconds if they are less than 10
+travail.innerHTML = `${Math.floor(travailTime / 60)}:${travailTime % 60 < 10 ? '0' : ''}${travailTime % 60}`;
+//display reposTime in repos and convert it to minutes and seconds
+//add a zero before the minutes and seconds if they are less than 10
+repos.innerHTML = `${Math.floor(reposTime / 60)}:${reposTime % 60 < 10 ? '0' : ''}${reposTime % 60}`;
+
+
+//addeventlistener to start button, click, begin the timer
 start.addEventListener('click', () => {
-    console.log("salut");
-    
-   let secondeDiminue = setInterval(() => {
-       
-        dateInit -= uneSeconde;
-        const tempsActualise = new Date(dateInit);
-        timer30.innerHTML = tempsActualise.toLocaleTimeString().slice(3);
-        console.log(dateInit);
-        if (dateInit === -3600000) {
-            
-            alert("Vous avez fini votre temps de travail !");
-            clearInterval(secondeDiminue);
-            
-            //stop the timer
-        } else if (dateInit < -3600000) {
-            clearInterval(secondeDiminue);
-            timer30.innerHTML = new Date((Date.now() - 1800000) - Date.now()).toLocaleTimeString().slice(3);
+    //create a variable for the interval
+    let interval = setInterval(() => {
+        //create a variable for the time
+        let time = document.querySelector('.time');
+        //create a variable for the minutes and seconds
+        let minutes = Math.floor(travailTime / 60);
+        let seconds = travailTime % 60;
+        //display the minutes and seconds in time
+        travail.innerHTML = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        //if the time is equal to 0, stop the interval and display a message
+        console.log(travailTime);
+        if (travailTime <= 0) {
+            clearInterval(interval);
+            travailTime = 10;
+            travail.innerHTML = `${Math.floor(travailTime / 60)}:${travailTime % 60 < 10 ? '0' : ''}${travailTime % 60}`;
+            //begin the repos timer
+            let interval2 = setInterval(() => {
+                let minutes = Math.floor(reposTime / 60);
+                let seconds = reposTime % 60;
+                repos.innerHTML = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+                if (reposTime <= 0) {
+                    clearInterval(interval2);
+                    reposTime = 5;
+                    repos.innerHTML = `${Math.floor(reposTime / 60)}:${reposTime % 60 < 10 ? '0' : ''}${reposTime % 60}`;
+                }
+                //decrease the reposTime by 1
+                reposTime--;
+            }, 1000);
         }
-
- 
-
+        //decrease the time by 1 second
+        travailTime--;
     }, 1000);
-    
 });
-
-
-
-
-// addeventlistener to stop button to stop the timer
