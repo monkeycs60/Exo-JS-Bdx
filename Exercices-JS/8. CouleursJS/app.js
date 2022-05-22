07fc87150c158297a24e9e28e1dc7ce9ffb5f4f3
monkeycs60 <clement.serizay@gmail.com>
2022-05-22 03:55:15 +0200

    mise en place js

---
+++ Exercices-JS/8. CouleursJS/app.js
@@ -0,0 +1,21 @@
+//create a const for couleur1
+const container = document.querySelector('.container');
+
+const couleur1 = document.getElementsByClassName('couleur1');
+const couleur2 = document.getElementsByClassName('couleur2');
+
+
+const colorFormula = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const colorFormula2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+couleur1[0].style.backgroundColor = colorFormula;
+couleur2[0].style.backgroundColor = colorFormula2;
+
+//add the text "clever" to couleur1
+document.querySelector('.couleur1').innerHTML = "gggf";
+
+console.log(colorFormula);
+console.log(colorFormula2);
+
+//set a linear gradient background for container with the value of colorFormula and colorFormula2
+container.style.background = `linear-gradient(to right, ${colorFormula}, ${colorFormula2})`;
