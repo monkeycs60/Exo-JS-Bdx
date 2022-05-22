22f42b8a9056aff7f40eb90ec0b8fd87eb60f879
monkeycs60 <clement.serizay@gmail.com>
2022-05-22 03:56:43 +0200

    dernière modif

--- Exercices-JS/8. CouleursJS/app.js
+++ Exercices-JS/8. CouleursJS/app.js
@@ -8,8 +8,8 @@ const couleur2 = document.getElementsByClassName('couleur2');
 const colorFormula = '#' + Math.floor(Math.random() * 16777215).toString(16);
 const colorFormula2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-couleur1[0].style.backgroundColor = colorFormula;
-couleur2[0].style.backgroundColor = colorFormula2;
+couleur1[0].style.background = colorFormula;
+couleur2[0].style.background = colorFormula2;
 
 //add the text "clever" to couleur1
 document.querySelector('.couleur1').innerHTML = "gggf";
