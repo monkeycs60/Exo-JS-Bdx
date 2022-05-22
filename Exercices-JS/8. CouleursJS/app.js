bfd8ef9251d0249a4468d9633f1416b172e3addf
monkeycs60 <clement.serizay@gmail.com>
2022-05-23 01:44:30 +0200

    bouton random

--- Exercices-JS/8. CouleursJS/app.js
+++ Exercices-JS/8. CouleursJS/app.js
@@ -5,30 +5,32 @@ const ensembleCouleurs = document.querySelector('.couleurs');
 const couleur1 = document.getElementsByClassName('couleur1');
 const couleur2 = document.getElementsByClassName('couleur2');
 
-
-const colorFormula = '#' + Math.floor(Math.random() * 16777215).toString(16);
+let colorFormula = '#' + Math.floor(Math.random() * 16777215).toString(16);
 const colorFormula2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+let colorTab = [colorFormula, colorFormula2];
+console.log(colorTab);
 
 const range = document.getElementById('range');
 const cursor = document.querySelector('.btn0');
 
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
 
-couleur1[0].style.background = colorFormula;
-couleur2[0].style.background = colorFormula2;
+couleur1[0].style.background = colorTab[0];
+couleur2[0].style.background = colorTab[1];
 
 
-document.querySelector('.couleur1').value += colorFormula;
-document.querySelector('.couleur2').value += colorFormula2;
+document.querySelector('.couleur1').value += colorTab[0];
+document.querySelector('.couleur2').value += colorTab[1];
 
 //change text color of couleur1 to colorFormula2
-couleur1[0].style.color = colorFormula2;
+couleur1[0].style.color = colorFormula[2];
 //change text color of couleur2 to colorFormula
-couleur2[0].style.color = colorFormula;
+couleur2[0].style.color = colorFormula[1];
 
 //set a linear gradient background for container with the value of colorFormula and colorFormula2
-container.style.background = `linear-gradient(to right, ${colorFormula}, ${colorFormula2})`;
+container.style.background = `linear-gradient(to right, ${colorTab})`;
 
 
 //when the range is changed, change the degree of the linear gradient
@@ -37,13 +39,13 @@ cursor.addEventListener('input', function() {
     console.log(cursor.value * -1 *7.2);
     //if cursor.value is less than 50, change the degree of the linear gradient
     if (cursor.value < 50) {
-        container.style.background = `linear-gradient(${cursor.value * -1 *7.2}deg, ${colorFormula}, ${colorFormula2})`;
+        container.style.background = `linear-gradient(${cursor.value * -1 *7.2}deg, ${colorTab})`;
     }
     else if (cursor.value === 50) {
-    container.style.background = `linear-gradient(${this.value}deg, ${colorFormula}, ${colorFormula2})`;
+    container.style.background = `linear-gradient(${this.value}deg, ${colorTab})`;
 }
     else {
-        container.style.background = `linear-gradient(${cursor.value *7.2}deg, ${colorFormula2}, ${colorFormula})`;
+        container.style.background = `linear-gradient(${cursor.value *7.2}deg, ${colorTab})`;
     }
 }
 );
@@ -52,11 +54,36 @@ cursor.addEventListener('input', function() {
 btn1.addEventListener('click', function() {
     const div = document.createElement('div');
     div.classList.add('couleur1');
-    div.style.background = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const couleurRandom = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    div.style.background = couleurRandom;
     //div.value is equal to its background
-    div.innerHTML +=  '#' + Math.floor(Math.random() * 16777215).toString(16);
+    div.innerHTML += couleurRandom;
+    colorTab.push(couleurRandom);
     ensembleCouleurs.appendChild(div);
     //add div.style.background to container.style.background linear gradient
-    container.style.background = `linear-gradient(to right, ${colorFormula}, ${colorFormula2}, ${div.style.background})`;
+   
+    
+    container.style.background = `linear-gradient(to right, ${colorTab})`;
+    //everytime you click btn1, add div.style.background to container linear gradient
+      
+}
+);
+
+//addeventlistener to btn2, when click remove the last div created with the class couleur1
+btn2.addEventListener('click', function() {
+    //you cannot remove the first two divs
+    if (ensembleCouleurs.childElementCount > 2) {
+    ensembleCouleurs.removeChild(ensembleCouleurs.lastChild);
+    colorTab.pop();
+    container.style.background = `linear-gradient(to right, ${colorTab})`;
+
+    
+}
+}
+);
+
+//btn3, when click, reload window   and reset the linear gradient
+btn3.addEventListener('click', function() {
+    window.location.reload();
 }
 );
